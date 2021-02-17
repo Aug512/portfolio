583ed5503db78008beb47c132536beb3bf50b9e2
Dmitriy Esko <Dmitriy.esko@gmail.com>
2021-02-17 12:34:13 +0300

    fixed projectsList title

--- src/components/ProjectsList/ProjectList.js
+++ src/components/ProjectsList/ProjectList.js
@@ -2,9 +2,12 @@ import React from 'react'
 import ProjectCard from '../ProjectCard/ProjectCard'
 
 export default function ProjectsList({ projects, lang }) {
+
+  const title = lang === 'RU' ? 'Текущие проекты' : 'Current projects'
+
   return(
     <div className='projectsList'>
-      <h2 className='projectsList__title'>Актуальные проекты:</h2>
+      <h2 className='projectsList__title'>{title}</h2>
       {projects.map(project => <ProjectCard key={project.id} data={project} lang={lang} />)}
     </div>
   )
